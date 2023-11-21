3aee6f7d4a01276da059478bf7d9cced36ad4ab7
Borja <brb0004@alu.medac.es>
2023-11-21 11:51:52 +0100

    Cambios proyecto 21/11

--- app/src/main/java/com/example/practica3pmm/SelectorZonaTrabajoActivity.java
+++ app/src/main/java/com/example/practica3pmm/SelectorZonaTrabajoActivity.java
@@ -17,6 +17,14 @@ public class SelectorZonaTrabajoActivity extends AppCompatActivity {
 
         Button btnZona1 = findViewById(R.id.btnZona1);
         Button btnZona2 = findViewById(R.id.btnZona2);
+        Button btnZona3 = findViewById(R.id.btnZona3);
+        Button btnZona4 = findViewById(R.id.btnZona4);
+        Button btnZona5 = findViewById(R.id.btnZona5);
+        Button btnZona6 = findViewById(R.id.btnZona6);
+        Button btnZona7 = findViewById(R.id.btnZona7);
+        Button btnZona8 = findViewById(R.id.btnZona8);
+        Button btnZona9 = findViewById(R.id.btnZona9);
+        Button btnZona10 = findViewById(R.id.btnZona10);
         Button btnSelectZona = findViewById(R.id.btnSelectZona);
 
         // Configurar listeners para cada botón
@@ -31,6 +39,58 @@ public class SelectorZonaTrabajoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 handleButtonClick(btnZona2);
+            }
+        });
+        btnZona3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona3);
+            }
+        });
+
+        btnZona4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona4);
+            }
+        });
+        btnZona5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona5);
+            }
+        });
+
+        btnZona6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona6);
+            }
+        });
+        btnZona7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona7);
+            }
+        });
+
+        btnZona8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona8);
+            }
+        });
+        btnZona9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona9);
+            }
+        });
+
+        btnZona10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnZona10);
             }
         });
 
