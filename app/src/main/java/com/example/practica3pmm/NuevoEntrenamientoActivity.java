61e8ab01d2edebc494c314977e8426dc85bb4784
Borja <brb0004@alu.medac.es>
2023-11-21 05:36:15 +0100

    Cambios proyecto 21/11

--- app/src/main/java/com/example/practica3pmm/NuevoEntrenamientoActivity.java
+++ app/src/main/java/com/example/practica3pmm/NuevoEntrenamientoActivity.java
@@ -18,6 +18,13 @@ public class NuevoEntrenamientoActivity extends AppCompatActivity {
         Button btnYoga1 = findViewById(R.id.btnYoga1);
         Button btnYoga2 = findViewById(R.id.btnYoga2);
         Button btnYoga3 = findViewById(R.id.btnYoga3);
+        Button btnYoga4 = findViewById(R.id.btnYoga4);
+        Button btnYoga5 = findViewById(R.id.btnYoga5);
+        Button btnYoga6 = findViewById(R.id.btnYoga6);
+        Button btnYoga7 = findViewById(R.id.btnYoga7);
+        Button btnYoga8 = findViewById(R.id.btnYoga8);
+        Button btnYoga9 = findViewById(R.id.btnYoga9);
+        Button btnYoga10 = findViewById(R.id.btnYoga10);
         Button btnSelectStyle = findViewById(R.id.btnSelectStyle);
 
         // Configurar listeners para cada botón
@@ -38,6 +45,48 @@ public class NuevoEntrenamientoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 handleButtonClick(btnYoga3);
+            }
+        });
+        btnYoga4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga4);
+            }
+        });
+        btnYoga5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga5);
+            }
+        });
+        btnYoga6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga6);
+            }
+        });
+        btnYoga7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga7);
+            }
+        });
+        btnYoga8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga8);
+            }
+        });
+        btnYoga9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga9);
+            }
+        });
+        btnYoga10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(btnYoga10);
             }
         });
 
